50dc2fedae54fa21f745bbfa8f222086ff4ce369
Skye <37989240+reamourad@users.noreply.github.com>
2024-02-17 15:42:27 -0500

    Created the Add  Category page

---
+++ app/src/main/java/com/example/smartflex/AddCategoryPage.java
@@ -0,0 +1,14 @@
+package com.example.smartflex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCategoryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category_page);
+    }
+}
