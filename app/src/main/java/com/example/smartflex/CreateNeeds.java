d38a9a253d95af30fe259dca92a289d9cef1128a
Skye <37989240+reamourad@users.noreply.github.com>
2024-02-17 15:53:38 -0500

    Linked Pages Together

--- app/src/main/java/com/example/smartflex/CreateNeeds.java
+++ app/src/main/java/com/example/smartflex/CreateNeeds.java
@@ -2,7 +2,11 @@ package com.example.smartflex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class CreateNeeds extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class CreateNeeds extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_needs);
+
+        LinearLayout btn = (LinearLayout) findViewById(R.id.addCategoryButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateNeeds.this, AddCategoryPage.class));
+            }
+        });
     }
 }
