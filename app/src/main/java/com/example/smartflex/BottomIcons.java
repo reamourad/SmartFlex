eee3be257d1627f4836966f08e5fdcbbbffa4e9d
Skye <mourad.rea@gmail.com>
2024-03-07 22:36:12 -0500

    Bottoms icons, not clickable yet

---
+++ app/src/main/java/com/example/smartflex/BottomIcons.java
@@ -0,0 +1,14 @@
+package com.example.smartflex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomIcons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_icons);
+    }
+}
